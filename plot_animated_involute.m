052bb71554383d690ebc457c932cb2df5cfd4453
Kirill Bikeev <47024516+defuntitled@users.noreply.github.com>
2023-06-08 10:55:24 +0300

    Update plot_animated_involute.m

--- plot_animated_involute.m
+++ plot_animated_involute.m
@@ -1,14 +1,12 @@
 function plot_animated_involute(curve)
-  t = linspace(0, 2*pi, 100);  % параметризация кривой
+  t = linspace(0, 2*pi, 100);  
   x = curve(t)';
-  dx = gradient(x);  % первая производная
-  ddx = gradient(dx);  % вторая производная
+  dx = gradient(x); 
+  ddx = gradient(dx);  
 
-  % построение графика кривой
   plot(x(:,1), x(:,2), 'b', 'LineWidth', 1);
   hold on;
 
-  % построение эвольвенты порциями (по 10 точек за раз)
   step = 10;
   for i = 1:step:length(t)-step
     K = (dx(i:i+step-1,1).*ddx(i:i+step-1,2) - ...
@@ -19,7 +17,6 @@ function plot_animated_involute(curve)
 
     h = plot(inv_x, inv_y, 'r', 'LineWidth', 1.5);
     pause(0.05);
-    delete(h);
   end
 
   hold off;
