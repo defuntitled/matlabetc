1096c3ceff9d409df57770f7424b4699c2d95e07
Kirill Bikeev <47024516+defuntitled@users.noreply.github.com>
2023-06-08 13:28:16 +0300

    Update plot_animated_involute.m

--- plot_animated_involute.m
+++ plot_animated_involute.m
@@ -1,24 +1,30 @@
-function plot_animated_involute(curve)
-  t = linspace(0, 2*pi, 100);  
-  x = curve(:,1)';
-  dx = gradient(x); 
-  ddx = gradient(dx);  
+function plot_animated_involute(x, y, start_t, end_t)
+	step = 0.01;
+	diff = @(f) @(t) (f(t + step) - f(t - step)) ./ (2 .* step);
+    dx_dt = diff(x);
+    dy_dt = diff(y);
+	helper = @(t) sqrt(dx_dt(t) .^ 2 + dy_dt(t) .^ 2);
+	multiplier = @(t) integral(helper, start_t, t) ./ helper(t);
+  involute_x = @(t) x(t) - dx_dt(t) .* multiplier(t);
+	involute_y = @(t) y(t) - dy_dt(t) .* multiplier(t);
 
-  plot(x(:,1), x(:,2), 'b', 'LineWidth', 1);
-  hold on;
-
-  step = 10;
-  for i = 1:step:length(t)-step
-    K = (dx(i:i+step-1,1).*ddx(i:i+step-1,2) - ...
-         dx(i:i+step-1,2).*ddx(i:i+step-1,1)) ./ ...
-        (dx(i:i+step-1,1).^2 + dx(i:i+step-1,2).^2).^0.5;
-    inv_x = x(i:i+step-1,1) - dx(i:i+step-1,2) ./ K;
-    inv_y = x(i:i+step-1,2) + dx(i:i+step-1,1) ./ K;
-
-    h = plot(inv_x, inv_y, 'r', 'LineWidth', 1.5);
-    pause(0.05);
-  end
-
-  hold off;
-endfunction
-
+	t = start_t:step:end_t;
+	inv_values_x = zeros(1, length(t));
+	inv_values_y = zeros(1, length(t));
+	for i = 1:length(t)
+		inv_values_x(i) = involute_x(t(i));
+		inv_values_y(i) = involute_y(t(i));
+	end
+    figure;
+    plot(x(t), y(t), 'b', 'LineWidth', 2);
+    hold on;
+    for i = 1:length(t)
+          scatter(inv_values_x(i), inv_values_y(i), 'r', 'LineWidth', 2);
+          pause(0.05);
+    endfor
+	hold off;
+    axis equal;
+    legend('curve', 'involute');
+    xlabel('x');
+    ylabel('y');
+end
